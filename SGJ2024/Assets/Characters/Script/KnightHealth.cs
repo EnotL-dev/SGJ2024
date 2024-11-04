ab299a8a14e4b33005ac29502e18d3133f4dac7d
Liho <84248601+LihoDev@users.noreply.github.com>
2024-11-04 09:03:48 +0300

    Added animations

--- SGJ2024/Assets/Characters/Script/KnightHealth.cs
+++ SGJ2024/Assets/Characters/Script/KnightHealth.cs
@@ -6,10 +6,24 @@ namespace BattleSystem
     {
         [SerializeField] private Battle _battle;
 
+
         protected override void Dead()
         {
+            _animator.SetTrigger("Dead");
             base.Dead();
             _battle.Defeat();
+        }
+
+        public override void TakeDamage(int value)
+        {
+            _animator.SetTrigger("Hit");
+            base.TakeDamage(value);
+        }
+
+        public override void TakeDamagePoison(int value)
+        {
+            _animator.SetTrigger("Hit");
+            base.TakeDamagePoison(value);
         }
 
         public void Poison(int damage)
